e6c1f6c96485e550e5cd8c267da89beb08337842
Gerlach, Andreas <Andreas.gerlach@dieboldnixdorf.com>
2017-04-05 10:51:10 +0200

    package updates

--- index.jsx
+++ index.jsx
@@ -4,6 +4,10 @@ import Home from './views/Home'
 import MainLayout from './views/MainLayout'
 import SettingsManager from './views/SettingsManager'
 
+exports.Components = {
+
+}
+
 exports.Views = {
   Home: Home,
   MainLayout: MainLayout,

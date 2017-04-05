078e24e931d4437985253f1a74d0fcab2677fd25
Gerlach, Andreas <Andreas.gerlach@dieboldnixdorf.com>
2017-04-05 17:04:14 +0200

    react drop zone for dropping extension files

--- views/ExtensionGallery.jsx
+++ views/ExtensionGallery.jsx
@@ -4,6 +4,10 @@ import React from 'react'
 import Radium from 'radium'
 import Reflux from 'reflux'
 
+import Dropzone from 'react-dropzone'
+import Icon from 'react-icons-kit'
+import { ic_file_upload } from 'react-icons-kit/md/ic_file_upload'
+
 import { FormattedMessage } from 'react-intl'
 
 class ExtensionGallery extends Reflux.Component {
@@ -11,6 +15,10 @@ class ExtensionGallery extends Reflux.Component {
   constructor(props, context) {
     super(props, context)
     this.stores = [context.extensionStore]
+  }
+
+  dropExtension (filesAccepted, filesRejected) {
+    console.log(filesAccepted, filesRejected)
   }
 
   render() {
@@ -22,6 +30,24 @@ class ExtensionGallery extends Reflux.Component {
             defaultMessage='Extension Gallery' />
         </h4>
         <div className='divider' />
+        <div style={{ height: '70vh', overflowY: 'auto' }}>
+          <div className='column col-12' style={{ overflowY: 'auto' }}>
+            <div className='card'>
+              <div className='card-body'>
+                <Dropzone key='dropHandler' onDrop={this.dropExtension} multiple={false} style={{ width: '100%', textAlign: 'center' }}>
+                  <div className='tile tile-centered'>
+                    <div className='tile-icon'>
+                      <Icon icon={ic_file_upload} size={32} />
+                    </div>
+                    <div className='tile-content'>
+                      <div className='tile-title'>Drop your new extensions here...</div>
+                    </div>
+                  </div>
+                </Dropzone>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }

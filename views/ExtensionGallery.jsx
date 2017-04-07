e7e18991ae53f89749ef1ed3d189b64622aca074
Andreas Gerlach <andi@appelgriebsch.org>
2017-04-07 16:26:18 +0200

    enhanced extension gallery view

--- views/ExtensionGallery.jsx
+++ views/ExtensionGallery.jsx
@@ -11,7 +11,7 @@ import { ic_delete } from 'react-icons-kit/md/ic_delete'
 
 import { FormattedMessage } from 'react-intl'
 
-import type { IExtensionManager } from 'electron-shell-lib'
+import type { ExtensionInfoType, IExtensionManager } from 'electron-shell-lib'
 
 class ExtensionGallery extends Reflux.Component {
 
@@ -21,6 +21,22 @@ class ExtensionGallery extends Reflux.Component {
     super(props, context)
     this.extensionManager = context.extensionManager
     this.stores = [context.extensionStore]
+  }
+
+  toogleExtensionStatus (extensionInfo: ExtensionInfoType) {
+    console.log(extensionInfo)
+    switch(extensionInfo.status) {
+      case 'active':
+        this.extensionManager.deactivate(extensionInfo.id)
+        break;
+      case 'deactive':
+        this.extensionManager.activate(extensionInfo.id)
+        break;
+    }
+  }
+
+  deleteExtension (extensionInfo: ExtensionInfoType) {
+    console.log(extensionInfo)
   }
 
   dropExtension (filesAccepted:Array<File>) {
@@ -37,28 +53,27 @@ class ExtensionGallery extends Reflux.Component {
             defaultMessage='Extension Gallery' />
         </h4>
         <div className='divider' />
-        <div style={{ height: '70vh', overflowY: 'auto' }}>
-          <div className='column col-12' style={{ overflowY: 'auto' }}>
-            <div className='card'>
-              <div className='card-body'>
-                <Dropzone key='dropHandler' onDrop={this.dropExtension.bind(this)} multiple={false} style={{ width: '100%' }}>
-                  <div className='tile tile-centered'>
-                    <div className='tile-icon'>
-                      <Icon icon={ic_file_upload} size={32} />
-                    </div>
-                    <div className='tile-content'>
-                      <div className='tile-title'>Drop your new extensions here...</div>
-                    </div>
-                  </div>
-                </Dropzone>
+        <div className='card' style={{ marginRight: '1em' }}>
+          <div className='card-body'>
+            <Dropzone key='dropHandler' onDrop={this.dropExtension.bind(this)} multiple={false} style={{ width: '100%' }}>
+              <div className='tile tile-centered'>
+                <div className='tile-icon'>
+                  <Icon icon={ic_file_upload} size={32} />
+                </div>
+                <div className='tile-content'>
+                  <div className='tile-title'>Drop your new extensions here...</div>
+                </div>
               </div>
-            </div>
-            <div className='divider' />
+            </Dropzone>
+          </div>
+        </div>
+        <div style={{ height: '60vh', overflowY: 'auto' }}>
+          <div className='column col-12' style={{ overflowY: 'auto' }}>
             {
               this.state.extensions.map((ext) => {
                 ext.isActivated = (ext.status === 'active')
                 return (
-                  <div className='card' key={ext._id}>
+                  <div className='card' key={ext._id} style={{ marginRight: '1em' }}>
                     <div className='card-image'>
                       <img className='img-responsive' src={ext.bannerImage} />
                     </div>
@@ -66,15 +81,23 @@ class ExtensionGallery extends Reflux.Component {
                       <div className='float-right' style={{ display: 'flex', flexDirection: 'row' }}>
                         <div className='form-group'>
                           <label className='form-switch'>
-                            <input type='checkbox' value={ext.isActivated} />
+                            <input type='checkbox' value={ext.isActivated} onChange={this.toogleExtensionStatus.bind(this, ext)} />
                             <i className='form-icon'></i> { ext.isActivated ? 'ACTIVE' : 'DEACTIVE' }
                           </label>
                         </div>
-                        <button className='btn btn-link' style={{ padding: 0 }}>
+                        <button className='btn btn-link' style={{ padding: 0 }} onClick={this.deleteExtension.bind(this, ext)}>
                           <Icon icon={ic_delete} size={24} />
                         </button>
                       </div>
                       <div className='card-title'>{ext.name}</div>
+                      <div className='card-subtitle'>
+                        <FormattedMessage id='app.settings.extgallery.author'
+                                          description='Shows the information line about the author of an extension'
+                                          defaultMessage='by {authorName}'
+                                          values={{
+                                            authorName: ext.author
+                                          }} />
+                      </div>
                     </div>
                     <div className='card-body'>
                       <div className='float-right'>
